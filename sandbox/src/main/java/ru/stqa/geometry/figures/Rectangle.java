362add3eb5736074ee4f90890dda2e3a001a4ae5
Vadim <bakshevsky.vadim@yandex.ru>
2024-04-01 14:44:37 +0500

    Изучение и повторение лекции 2.4

--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -1,6 +1,8 @@
 package ru.stqa.geometry.figures;
 
-public record Rectangle(double a,double b ) {
+import java.util.Objects;
+
+public record Rectangle(double a, double b ) {
 
     public  Rectangle {
         if (a < 0 || b < 0) {
@@ -14,5 +16,19 @@ public record Rectangle(double a,double b ) {
 
     private double rectangleArea() {
         return this.a * this.b;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return Double.compare(this.a, rectangle.a) == 0 && Double.compare(this.b, rectangle.b) == 0
+                ||Double.compare(this.b, rectangle.a) == 0 && Double.compare(this.a, rectangle.b) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b);
     }
 }
