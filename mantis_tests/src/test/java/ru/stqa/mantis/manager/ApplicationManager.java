219bf5d3b70a4537690fee076a2d8a7b26465a01
Vadim <bakshevsky.vadim@yandex.ru>
2024-05-20 17:26:52 +0500

    Повторение лекции 9.6

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -19,6 +19,7 @@ public class ApplicationManager {
     private JamesApiHelper jamesApiHelper;
     private DeveloperMailHelper developerMailHelper;
     private RestApiHelper restApiHelper;
+    private SoapApiHelper soapApiHelper;
 
     public void init(String browser, Properties properties) {
         this.browser = browser;
@@ -91,7 +92,12 @@ public class ApplicationManager {
         }
         return restApiHelper;
     }
-
+    public SoapApiHelper soap() {
+        if (soapApiHelper == null) {
+            soapApiHelper = new SoapApiHelper(this);
+        }
+        return soapApiHelper;
+    }
     public String property(String name) {
         return properties.getProperty(name);
     }
